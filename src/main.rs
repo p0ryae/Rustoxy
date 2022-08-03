53d1f520f5e970536ca1928c1b1ababd7e8f40ba
DashCruft <theporyafm12@gmail.com>
2022-08-02 20:43:48 -0700

    guild_member_addition

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use serenity::async_trait;
+use serenity::builder::CreateEmbed;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{CommandResult, StandardFramework};
 use serenity::model::application::interaction::Interaction;
 use serenity::model::channel::Message;
+use serenity::model::guild::Member;
 use serenity::model::prelude::command::Command;
 use serenity::model::prelude::Ready;
-use serenity::builder::CreateEmbed;
 use serenity::prelude::*;
 use std::env;
 use std::time::Instant;
+use rand::Rng; 
 
 #[group]
 #[commands(ping)]
@@ -48,13 +50,15 @@ impl EventHandler for Handler {
                 "bins" => {
                     let mut embed = CreateEmbed::default();
                     embed.title("Paste your code in one of the following websites");
-                    embed.description("> [__`SourceBin`__](https://sourceb.in/)
+                    embed.description(
+                        "> [__`SourceBin`__](https://sourceb.in/)
                     > [__`HasteBin`__](https://hasteb.in/)
                     > [__`HateBin`__](https://hatebin.com/)
                     > [__`Hastebin`__](https://hastebin.com/)
-                    > [__`PasteBin`__](https://pastebin.com/)");
+                    > [__`PasteBin`__](https://pastebin.com/)",
+                    );
                     embed.color((47, 49, 54));
-                    embed.footer(|f| {f.text("Just use SourceBin for bobux 😳")});
+                    embed.footer(|f| f.text("Just use SourceBin for bobux 😳"));
 
                     msg_interaction
                         .create_interaction_response(&ctx.http, |response| {
@@ -76,6 +80,33 @@ impl EventHandler for Handler {
             };
         }
     }
+    async fn guild_member_addition(&self, ctx: Context, new_member: Member) {
+        let guild_id = new_member.guild_id;
+        if let Ok(guild) = guild_id.to_partial_guild(&ctx).await {
+            let channels = guild.channels(&ctx).await.unwrap();
+            let channel_search = channels.values().find(|c| c.name == "general");
+            if let Some(channel) = channel_search {
+                let num = rand::thread_rng().gen_range(0..=255);
+
+                let _ = channel
+                    .send_message(&ctx, |m| {
+                        m.embed(|e| {
+                            if let Some(ref joined_at) = new_member.joined_at {
+                                e.timestamp(joined_at);
+                            }
+                            e.description(format!(
+                                "**{}**, joined DashCruft Nation!",
+                                new_member.display_name()
+                            ));
+                            e.color((num, num, num));
+                            e.footer(|f| f.text("Welcome!"))
+
+                        })
+                    })
+                    .await;
+            }
+        }
+    }
 }
 
 #[tokio::main]
@@ -88,7 +119,8 @@ async fn main() {
     let intents = GatewayIntents::non_privileged()
         | GatewayIntents::MESSAGE_CONTENT
         | GatewayIntents::GUILDS
-        | GatewayIntents::GUILD_MESSAGES;
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::GUILD_MEMBERS;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .framework(framework)
